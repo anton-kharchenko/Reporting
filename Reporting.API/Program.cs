d798b11f467e7b049e01ceeb649a29fb93f49278
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2024-10-27 19:19:59 +0200

    feat(solution): init GraphQL project
    add a project and HotChocolate library

---
+++ Reporting.API/Program.cs
@@ -0,0 +1,25 @@
+using Reporting.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGraphQL();
+
+app.Run();
---
+++ Reporting.API/Query.cs
@@ -0,0 +1,13 @@
+﻿namespace Reporting.API;
+
+public class Query
+{
+    public Book GetBook()
+    {
+        return new Book("title", new Author("Anton Kharchenko"));
+    }
+}
+
+public record Book(string Title, Author Author);
+
+public record Author(string Name);
